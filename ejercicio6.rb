6f0aca13620be5a04b1369d84184201ce72abfa0
Marcelo <marcelo.onate@gmail.com>
2018-08-06 21:19:00 -0400

    add:ejercicio6

--- ejercicio6.rb
+++ ejercicio6.rb
@@ -1,18 +1,61 @@
-#Se tiene el archivo config.ru :
-
-#config.ru
-require 'rack'
-class MiPrimeraWebApp
- def call(env)
- [000, {}, []]
- end
-end
-run MiPrimeraWebApp.new
-
 =begin
-Se pide:
-Agregar un código de respuesta 200.
-Agregar en los Response Headers un Content-type de tipo text/html.
-Agregar en el Response Body una etiqueta de párrafo que contenga un texto Lorem
-ipsum
+	
+## Ejercicio 6: Mixins.
+
+- Crear una clase **Student** con un método constructor que recibirá 3 argumentos 
+(nombre, nota1 y nota2). Cada una de las notas tendrá, por defecto, valor **4** en 
+el caso que no se ingrese una nota al momento de crear una nueva instancia.
+ 
+- La clase también debe tener una variable de clase llamada **quantity** la que 
+será iniciada en 0 y se incrementará en 1 cada vez que se instancie un nuevo objeto.
+
+- Crear un módulo **Test** con un método **result**. Este método debe calcular el promedio de 2 notas 
+y si ese promedio es superior a 4 entregrar un mensaje que debe decir **"Estudiante aprobado"**.
+ En caso contrario, enviará un mensaje **"Estudiante reprobado"**.
+
+- Crear un segundo módulo **Attendance** con un método **student_quantity** que permita mostrar 
+en pantalla la cantidad de estudiantes creados.
+
+ > Añadir a la clase Student el módulo Test como métodos de instancia y el módulo Attendance como 
+ métodos de clase.
+ 
+- Crear 10 objetos de la clase Student y utilizar los métodos creados para saber si los alumnos 
+están aprobados o no y, finalmente, mostrar el total de alumnos creados.
+
 =end
+module Test
+		def result
+		average = (nota1 + nota2) / 2.0
+		puts average > 4 ? "Estudiante Aprobado" : "Estudiante reprobado"
+		end
+end
+
+module Attendance
+	def student_quantity
+		puts "Estudiantes creados: #{@quantity}"
+	end
+end
+
+class Student
+	include Test
+	extend Attendance
+
+	attr_accessor :nota1, :nota2
+	@@quantity = 0
+	def initialize(nombre, nota1 = 4, nota2 = 4)
+		@nombre = nombre
+		@nota1 = nota1
+		@nota2 = nota2
+		@@quantity += 1
+
+	end
+
+	def self.quantity
+		@@quantity
+	end
+end
+
+students = [['Pipe', 6, 3], ['Marcelo', 4, 3], ['Pedro', 6, 7]]
+
+students.each { |s| Student.new(*s).result }
+student.student_quantity
---
+++ ejercicio6/config.ru
@@ -0,0 +1,19 @@
+
+#config.ru
+require 'rack'
+
+class MiPrimeraWebApp
+ def call(env)
+  [200, {'Content-type' =>'text/html'}, ["<p>Lorem ipsum</p>"]]
+ end
+end
+
+run MiPrimeraWebApp.new
+
+
+#Agregar un código de respuesta 200.
+#Agregar en los Response Headers un Content-type de tipo text/html.
+#Agregar en el Response Body una etiqueta de párrafo que contenga un texto Lorem ipsum.
+
+
+
